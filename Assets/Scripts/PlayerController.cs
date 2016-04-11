0e5f7782ecc33af65ae0b54885e94d5c95784976
Ray Fung <rayfung1013@gmail.com>
2016-04-11 11:38:17 -0700

    Fighting Scene Started

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Movement variables
+	public float speed;
+	public float jump;
+	float moveVelocity;
+
+	//Boolean to see if on the ground
+	bool grounded = true;
+
+	// Use this for initialization
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.W)) 
+		{
+			if(grounded)
+			{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+			}
+		}
+
+		moveVelocity = 0;
+
+		//Left and right movement
+
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			moveVelocity = -speed;
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			moveVelocity = speed;
+		}
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
+	}
+
+	//Method to check if character is on the ground
+
+	void OnTriggerEnter2D()
+	{
+		grounded = true;
+	}
+	void OnTriggerExit2D()
+	{
+		grounded = false;
+	}
+}
