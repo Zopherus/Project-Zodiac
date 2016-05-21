2a8b090f8c5e27f3c6223aa1c4f526197b75548c
Ray Fung <rayfung1013@gmail.com>
2016-05-21 12:59:01 -0700

    Health bars + Trump Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,24 +12,34 @@ public class PlayerController : MonoBehaviour {
 
 	//Boolean to see if on the ground
 	bool grounded = true;
+	Animator animator;
+	bool direction;
+	bool moving;
 
 	void Start()
 	{
-		sprite = GameObject.Find ("Panel");
+		animator = GetComponent<Animator> ();
+		direction = true;
 	}
 
 	void Update () 
 	{
-		RectTransform transform = sprite.GetComponent<RectTransform> ();
-	
-		transform.position = this.GetComponent<RectTransform> ().position;
 		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.W)) 
 		{
-			if(grounded)
+			if(grounded & direction)
 			{
 				GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+				animator.SetTrigger ("jumpRight");
+			}
+
+			if (grounded & !direction)
+			{
+				GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+				animator.SetTrigger ("jumpLeft");
 			}
 		}
+			
+		animator.SetBool ("onGround", grounded);
 
 		moveVelocity = 0;
 
@@ -38,11 +48,39 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
 			moveVelocity = -speed;
+
+			animator.SetTrigger ("faceLeft");
+			animator.SetTrigger ("movingStart");
+
+			moving = true;
+			animator.SetBool ("moving", moving);
+
+			direction = false;
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.A)) 
+		{
+			moving = false;
+			animator.SetBool ("moving", moving);
 		}
 
 		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 		{
 			moveVelocity = speed;
+
+			animator.SetTrigger ("faceRight");
+			animator.SetTrigger ("movingStart");
+
+			moving = true;
+			animator.SetBool ("moving", moving);
+
+			direction = true;
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow) || Input.GetKeyUp (KeyCode.D)) 
+		{
+			moving = false;
+			animator.SetBool ("moving", moving);
 		}
 
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
